9f3f276e00f02802d4ce77caa28d1235e0aa911a
ramkumar912 <mramkumar912@gmail.com>
2023-07-28 19:43:52 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+const  isLoggedIn ={
+  status:0
+}
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleClick}>
+    Click me
+  </button>
+  );
+}
+function LoginForm() {
+  return (
+    <h4>This login form</h4>
+  );
+}
+function AdminPanel() {
+  return (
+    <h4>This Admin Panel</h4>
+  );
+}
+export default function MyApp() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  return (
+    <>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
+   
+    <h1>{user.name}</h1>
+    <img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'Photo of ' + user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize
+      }}
+    />
+<div>
+  {isLoggedIn.status == 1 ? (
+    <AdminPanel />
+  ) : (
+    <LoginForm />
+  )}
+
+</div>
+
+
+    <ul>{listItems}</ul>
+
+  </>
   );
 }
 
-export default App;
+//export default App;
